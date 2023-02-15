ba9295a6ab6835cc45fd46a4c1a8bbd2a471bd53
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-15 11:50:28 -0500

    Switch v/s nested if else Switch does not work on all datatype

--- src/SwitchExcercisesRunner.java
+++ src/SwitchExcercisesRunner.java
@@ -1,41 +1,53 @@
- public class SwitchExcercisesRunner {
-     public static void main(String[] args) {
+public class SwitchExcercisesRunner {
+    public static void main(String[] args) {
+
+
+        System.out.println(determineNameOfDay(2));
+        System.out.println(isWeekday(2));
+    }
+
+    public static boolean isWeekday(int dayNumber) {
+        switch (dayNumber) {
+            case 0, 6: //merge cases that are false
+                return false;
+            case 1, 2, 3, 4, 5://merge cases that are true
+                return true;
+        }
+        return false;
+    }
 
 
 
-             System.out.println(determineNameOfDay(2));
-         }
+    public static String determineNameOfDay(int dayNumber) {
 
-             public static String determineNameOfDay ( int dayNumber){
+        switch (dayNumber) {
+            case 0:
+                return "Sunday";
 
-             switch (dayNumber) {
-                 case 0:
-                     return "Sunday";
+            case 1:
+                return "Monday";
 
-                 case 1:
-                     return "Monday";
+            case 2:
+                return "Tuesday";
 
-                 case 2:
-                     return "Tuesday";
+            case 3:
+                return "Wednesday";
 
-                 case 3:
-                     return "Wednesday";
+            case 4:
+                return "Thursday";
 
-                 case 4:
-                     return "Thursday";
+            case 5:
+                return "Friday";
 
-                 case 5:
-                     return "Friday";
+            case 6:
+                return "Saturday";
 
-                 case 6:
-                     return "Saturday";
-
-             }
-             return "Invalid Day";
-         }
+        }
+        return "Invalid Day";
+    }
 
 
-         }
+}
 
 
 
