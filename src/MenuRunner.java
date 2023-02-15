eaa155d49a2dbc18ba83df430b5e123be205d783
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-15 09:51:17 -0500

    Switch v/s nested if else

--- src/MenuRunner.java
+++ src/MenuRunner.java
@@ -26,7 +26,30 @@ public class MenuRunner {
         System.out.println("Number1 " + number1);
         System.out.println("Number2 " + number2);
         System.out.println("Choice " + choice);
-        if (choice == 1) {
+        performOperationsUsingSwitch(number1, number2, choice);
+    }
+
+    private static void performOperationsUsingSwitch(int number1, int number2, int choice) {
+        switch (choice) {
+            case 1:
+                System.out.println("Result " + (number1 + number2));
+                break;
+            case 2:
+                System.out.println("Result " + (number1 - number2));
+                break;
+            case 3:
+                System.out.println("Result " + (number1 * number2));
+                break;
+            case 4:
+                System.out.println("Result " + (number1 / number2));
+                break;
+            default:
+                System.out.println("Invalid Operation");
+                break;
+        }
+    }
+}
+        /*if (choice == 1) {
             System.out.println("Result " + (number1 + number2));
         } else if (choice == 2) {
             System.out.println("Result " + (number1 - number2));
@@ -40,4 +63,4 @@ public class MenuRunner {
 
         }
     }
-}
+}*/
